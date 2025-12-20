5d398b7887f57a87cdd7c46ef3cb21d3f5f18ab5
Malcolm Almuntazar-Harris <almuntazar.harris.malcolm@gmail.com>
2025-12-20 11:13:01 -0500

    git commit -m "add project scaffolding"

---
+++ infra/aws_setup_notes.md
@@ -0,0 +1,5 @@
+AWS_PROFILE=default
+AWS_REGION=us-east-2
+S3_BUCKET=nyc-taxi-bucket-mah
+S3_PREFIX=tlc/raw
+RUN_DATE=2025-12-20
---
+++ src/common/logging.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    
+    logger.setlevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        fmt="%(asctime)s %(levelname)s %(name)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
